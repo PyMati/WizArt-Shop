2d9cc8498c1b7bae7f70e749ed71a873be85adc4
PyMati <mateuszcodedeveloper@gmail.com>
2023-10-28 13:22:05 +0200

    Improving logging logic

--- backend/shop/shopapi/utils.py
+++ backend/shop/shopapi/utils.py
@@ -1,4 +1,5 @@
 from datetime import date
+from django.utils import timezone
 import os
 
 
@@ -6,10 +7,10 @@ def saveActionToLogFile(method: str, message: str):
     try:
         with open(f"./shopapi/logs/{date.today()}-log.txt", "a") as logfile:
             logfile.write(
-                f"{method}:{message}:{date.today().strftime('%H:%M:%S')}\n")
+                f"{method}:{message}:{timezone.now().strftime('%H:%M:%S')}\n")
     except FileNotFoundError:
         new_file = open(f"./shopapi/logs/{date.today()}-log.txt", "w")
         new_file.write(
-            f"File created:{date.today().strftime('%H:%M:%S')}\n{method}:{message}:{date.today().strftime('%H:%M:%S')}\n")
+            f"File created:{timezone.now().strftime('%H:%M:%S')}\n{method}:{message}:{timezone.now().strftime('%H:%M:%S')}\n")
         new_file.close()
         print("File created and log was saved.")
